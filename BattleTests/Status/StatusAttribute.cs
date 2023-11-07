6e71196731b1f2d3b5278a6514a40a3c1ce997cd
Sprocket <lpolar45@gmail.com>
2023-11-06 23:21:53 -0500

    oops

--- BattleTests/Status/StatusAttribute.cs
+++ BattleTests/Status/StatusAttribute.cs
@@ -3,5 +3,10 @@ namespace BattleTests;
 [System.AttributeUsage(System.AttributeTargets.Method)]
 public class StatusAttribute : System.Attribute
 {
-    
+    public string Name;
+
+    public StatusAttribute(string name)
+    {
+        Name = name;
+    }
 }
